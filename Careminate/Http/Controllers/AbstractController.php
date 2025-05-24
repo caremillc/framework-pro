62eca16341dad555ab6ad310ef0ef7807fe0c499
caremi <Willy Nilly>
2025-05-24 16:27:51 +0400

    Added AbstractController to Container

--- Careminate/Http/Controllers/AbstractController.php
+++ Careminate/Http/Controllers/AbstractController.php
@@ -1,7 +1,14 @@
 <?php declare(strict_types=1);
 namespace Careminate\Http\Controllers;
 
+use Psr\Container\ContainerInterface;
+
 abstract class AbstractController
 {
-    
+    protected ?ContainerInterface $container = null;
+
+    public function setContainer(ContainerInterface $container): void
+    {
+        $this->container = $container;
+    }
 }
