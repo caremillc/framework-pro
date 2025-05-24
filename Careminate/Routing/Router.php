6390f080576e0a93792ffdbaf4c080e48aabf9f7
caremi <Willy Nilly>
2025-05-24 15:22:28 +0400

    Container Parameters

--- Careminate/Routing/Router.php
+++ Careminate/Routing/Router.php
@@ -10,38 +10,46 @@ use function FastRoute\simpleDispatcher;
 
 class Router implements RouterInterface
 {
+    private array $routes;
+
+    public function setRoutes(array $routes): void
+    {
+        //$routes is parsed from setRoutes in $container
+        $this->routes = $routes;
+    }
+
     public function dispatch(Request $request): array
     {
         $routeInfo = $this->extractRouteInfo($request);
 
-        // If routeInfo is null (e.g., for favicon or similar), short-circuit gracefully
+        // If favicon or null handler
         if ($routeInfo === null) {
             return [[fn() => new \Careminate\Http\Responses\Response('', 204), '__invoke'], []];
         }
 
         [$handler, $vars] = $routeInfo;
 
+        // Validate the handler
         if (! is_array($handler) || ! is_string($handler[0]) || ! is_string($handler[1])) {
             throw new \InvalidArgumentException('Invalid route handler definition.');
         }
 
         [$controller, $method] = $handler;
 
-        return [[new $controller, $method], $vars];
+        return [[new $controller(), $method], $vars];
     }
-
 
     private function extractRouteInfo(Request $request): array | null
     {
         $requestedPath = $request->getPathInfo();
 
-           // Ignore requests for favicon.ico
-       if ($requestedPath === '/favicon.ico') {
-           return [null, []]; // Return a no-op response
-       }
+        if ($requestedPath === '/favicon.ico') {
+            return null;
+        }
 
         $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
-            $routes = require_once route_path('web.php');
+            $routes = require route_path('web.php');
+
             foreach ($routes as $route) {
                 $routeCollector->addRoute(...$route);
             }
@@ -59,7 +67,7 @@ class Router implements RouterInterface
                 $allowedMethods = implode(', ', $routeInfo[1]);
                 throw new HttpRequestMethodException("The allowed methods are $allowedMethods", 405);
             default:
-                throw new HttpException('Not found', 404);
+                throw new HttpException('Not Found', 404);
         }
     }
 
