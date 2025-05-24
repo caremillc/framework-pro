277e969b113ac4170eb7d551dcd29b3b1a245cb9
caremi <Willy Nilly>
2025-05-24 13:33:50 +0400

    Adding Routes

--- Careminate/Http/Kernel.php
+++ Careminate/Http/Kernel.php
@@ -1,6 +1,9 @@
-<?php declare(strict_types=1); 
+<?php declare(strict_types=1);
 namespace Careminate\Http;
 
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
 use Careminate\Http\Requests\Request;
 use Careminate\Http\Responses\Response;
 
@@ -8,9 +11,34 @@ class Kernel
 {
     public function handle(Request $request): Response
     {
-         $content = '<h1>Hello World from Http Kernel</h1>';
+        // Create a dispatcher
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
+            $routeCollector->addRoute('GET', '/', function() {
+                $content = '<h1>Hello World</h1>';
 
-        return new Response($content);
-        
+                return new Response($content);
+            });
+
+            $routeCollector->addRoute('GET', '/posts/{id:\d+}/show', function($routeParams) {
+                $content = "<h1>This is Post {$routeParams['id']}</h1>";
+
+                return new Response($content);
+            });
+        });
+
+//    dd($dispatcher);
+
+        // Dispatch a URI, to obtain the route info
+        $routeInfo = $dispatcher->dispatch(
+            $request->getMethod(),
+            $request->getPathInfo()
+        );
+
+   dd($routeInfo);
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        // Call the handler, provided by the route info, in order to create a Response
+        return $handler($vars);
     }
 }
