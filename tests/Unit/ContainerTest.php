76553fb4aa3bea40d5ca13feaf480eb5315e2ac3
caremi <Willy Nilly>
2025-05-24 14:47:01 +0400

    Autowiring

--- tests/Unit/ContainerTest.php
+++ tests/Unit/ContainerTest.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php  declare(strict_types=1);
 namespace Careminate\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
@@ -7,6 +7,7 @@ use Careminate\Exceptions\ContainerException;
 
 class ContainerTest extends TestCase
 {
+
     public function test_a_service_can_be_retrieved_from_the_container()
     {
         // Setup
@@ -20,7 +21,6 @@ class ContainerTest extends TestCase
         $this->assertInstanceOf(DependantClass::class, $container->get('dependant-class'));
     }
 
-  
     public function test_a_ContainerException_is_thrown_if_a_service_cannot_be_found()
     {
         // Setup
@@ -33,7 +33,6 @@ class ContainerTest extends TestCase
         $container->add('foobar');
     }
 
-   
     public function test_can_check_if_the_container_has_a_service(): void
     {
         // Setup
@@ -44,6 +43,18 @@ class ContainerTest extends TestCase
 
         $this->assertTrue($container->has('dependant-class'));
         $this->assertFalse($container->has('non-existent-class'));
+    }
+
+    public function test_services_can_be_recursively_autowired()
+    {
+        $container = new Container();
+
+        $dependantService = $container->get(DependantClass::class);
+
+        $dependancyService = $dependantService->getDependency();
+
+        $this->assertInstanceOf(DependencyClass::class, $dependancyService);
+        $this->assertInstanceOf(SubDependencyClass::class, $dependancyService->getSubDependency());
     }
 
     public function test_it_works()
