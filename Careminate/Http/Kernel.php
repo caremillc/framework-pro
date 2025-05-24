39cc1500673fa20f7b40d4afe1156436d7719ae9
caremi <Willy Nilly>
2025-05-24 15:46:08 +0400

    add environment variables

--- Careminate/Http/Kernel.php
+++ Careminate/Http/Kernel.php
@@ -9,18 +9,33 @@ use Careminate\Exceptions\HttpException;
 
 class Kernel
 {
+    private string $appEnv;
+    private string $appKey;
+    private string $appVersion;
+
     public function __construct(
         private RouterInterface $router,
         private ContainerInterface $container
-    )
-    {
+    ){
+        // Check .env file and configuration values
+        if (!file_exists('.env') || !is_readable('.env')) {
+            throw new \RuntimeException('.env file is missing or not readable.');
+        }
+
+        $this->appEnv = $this->container->get('APP_ENV');
+        $this->appKey = $this->container->get('APP_KEY');
+        $this->appVersion = $this->container->get('APP_VERSION');
+
+        if (empty($this->appKey) || empty($this->appEnv) || empty($this->appVersion)) {
+            throw new \RuntimeException('One or more required environment variables are missing.');
+        }
     }
 
     public function handle(Request $request): Response
     {
         try {
 
-            [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
+           [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
 
             
              // Validate that the routeHandler is actually callable
@@ -36,9 +51,27 @@ class Kernel
             }
 
         } catch (HttpException $exception) {
-            $response = new Response($exception->getMessage(), $exception->getStatusCode());
+            $response = $this->createExceptionResponse($exception);
         }
 
         return $response;
     }
+
+     private function createExceptionResponse(\Exception $exception): Response
+	{
+		// Check if the environment is development or local testing
+		if (in_array($this->appEnv, ['dev', 'local', 'test'])) {
+			// In development or local testing, rethrow the exception for detailed debugging
+			throw $exception;
+		}
+
+		// Production environment handling
+		if ($exception instanceof HttpException) {
+			// Return a response with the HTTP status and message for HTTP exceptions
+			return new Response($exception->getMessage(), $exception->getStatusCode());
+		}
+
+		// For all other exceptions, return a generic server error message
+		return new Response('Server error', Response::HTTP_INTERNAL_SERVER_ERROR);
+	}
 }
--- Careminate/Support/helpers.php
+++ Careminate/Support/helpers.php
@@ -1,9 +1,9 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types = 1);
 
 // Just include the file at the top of your script
 require_once 'debug_functions.php';
 
-if (!function_exists('value')) {
+if (! function_exists('value')) {
     /**
      * Return the default value of the given value.
      *
@@ -17,18 +17,18 @@ if (!function_exists('value')) {
     }
 }
 
-if (!function_exists('base_path')) {
+if (! function_exists('base_path')) {
     function base_path(?string $file = null)
     {
-        return  ROOT_DIR. '/../' . $file;
+        return ROOT_DIR . '/../' . $file;
     }
 }
 
-if (!function_exists('config')) {
+if (! function_exists('config')) {
     function config(?string $file = null)
     {
         $seprate = explode('.', $file);
-        if ((!empty($seprate) && count($seprate) > 1) && !is_null($file)) {
+        if ((! empty($seprate) && count($seprate) > 1) && ! is_null($file)) {
             $file = include base_path('config/') . $seprate[0] . '.php';
             return isset($file[$seprate[1]]) ? $file[$seprate[1]] : $file;
         }
@@ -36,9 +36,42 @@ if (!function_exists('config')) {
     }
 }
 
-if (!function_exists('route_path')) {
+if (! function_exists('route_path')) {
     function route_path(?string $file = null)
     {
-        return !is_null($file) ? config('route.path') . '/' . $file : config('route.path');
+        return ! is_null($file) ? config('route.path') . '/' . $file : config('route.path');
+    }
+}
+
+// Env Function
+if (! function_exists('env')) {
+    /**
+     * Get an environment variable, or return the default value if not found.
+     *
+     * Supports various data types.
+     *
+     * @param string $key The name of the environment variable.
+     * @param mixed $default The default value to return if the environment variable is not found.
+     * @return mixed The value of the environment variable or the default value.
+     */
+    function env(string $key, $default = null)
+    {
+        $value = $_ENV[$key] ?? $_SERVER[$key] ?? getenv($key) ?: $default;
+
+        if (! is_string($value)) {
+            return $value;
+        }
+
+        $trimmedValue = trim($value);
+
+        return match (strtolower($trimmedValue)) {
+            'true' => true,
+            'false' => false,
+            'null' => null,
+            'empty' => '',
+            default => is_numeric($trimmedValue) ? (str_contains($trimmedValue, '.') ? (float) $trimmedValue : (int) $trimmedValue) : (
+                preg_match('/^[\[{].*[\]}]$/', $trimmedValue) ? (json_decode($trimmedValue, true) ?? $trimmedValue) : $trimmedValue
+            )
+        };
     }
 }
