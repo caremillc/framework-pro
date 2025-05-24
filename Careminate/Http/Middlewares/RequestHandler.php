78a495d5e74869f709e84ec6e022652486795502
caremi <Willy Nilly>
2025-05-24 23:12:47 +0400

    Resolving Middleware from the container

--- Careminate/Http/Middlewares/RequestHandler.php
+++ Careminate/Http/Middlewares/RequestHandler.php
@@ -2,6 +2,7 @@
 namespace Careminate\Http\Middlewares;
 
 use Careminate\Http\Requests\Request;
+use Psr\Container\ContainerInterface;
 use Careminate\Http\Responses\Response;
 use Careminate\Http\Middlewares\Contracts\RequestHandlerInterface;
 
@@ -12,6 +13,8 @@ class RequestHandler implements RequestHandlerInterface
         SuccessMiddleware::class
     ];
 
+    public function __construct(private ContainerInterface $container){}  //step 1
+     
     public function handle(Request $request): Response
     {
         // If there are no middleware classes to execute, return a default response
@@ -23,10 +26,13 @@ class RequestHandler implements RequestHandlerInterface
         // Get the next middleware class to execute
         $middlewareClass = array_shift($this->middleware);
 
+        // Get middleware from container
+        $middleware = $this->container->get($middlewareClass);  // step 2
+
         // Create a new instance of the middleware call process on it
-        $response = (new $middlewareClass())->process($request, $this);
+        // $response = (new $middlewareClass())->process($request, $this);
+        $response = $middleware->process($request, $this); // step 3
 
         return $response;
     }
 }
-
