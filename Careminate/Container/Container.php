2011a20957b2999aafb61a2c77efdb76c675d40a
caremi <Willy Nilly>
2025-05-24 14:50:38 +0400

    Using a Container

--- Careminate/Container/Container.php
+++ Careminate/Container/Container.php
@@ -1,4 +1,4 @@
-<?php  declare(strict_types=1);
+<?php declare(strict_types=1);
 namespace Careminate\Container;
 
 use Psr\Container\ContainerInterface;
@@ -6,87 +6,167 @@ use Careminate\Exceptions\ContainerException;
 
 class Container implements ContainerInterface
 {
-    private array $container = [];
+    protected array $bindings = [];
+    protected array $singletons = [];
+    protected array $aliases = [];
+    protected array $parameters = [];
+    protected array $resolved = [];
 
-    public function add(string $id, string|object|null $concrete = null)
+    public function __construct(array $config = [])
     {
-        if ($concrete === null) {
-            if (!class_exists($id)) {
-                throw new ContainerException("Service $id could not be added");
-            }
-
-            $concrete = $id;
-        }
-
-        $this->container[$id] = $concrete;
+        $this->parameters = $config;
     }
 
     public function get(string $id)
     {
-        if (!$this->has($id)) {
-            if (!class_exists($id)) {
-                throw new ContainerException("Service $id could not be resolved");
-            }
-
-            $this->add($id);
+        if (isset($this->aliases[$id])) {
+            return $this->get($this->aliases[$id]);
         }
 
-        $object = $this->resolve($this->container[$id]);
-
-        return $object;
-    }
-
-    private function resolve($class): object
-    {
-        // 1. Instantiate a Reflection class (dump and check)
-        $reflectionClass = new \ReflectionClass($class);
-
-        // 2. Use Reflection to try to obtain a class constructor
-        $constructor = $reflectionClass->getConstructor();
-
-        // 3. If there is no constructor, simply instantiate
-        if (null === $constructor) {
-            return $reflectionClass->newInstance();
+        // Check if it's a singleton
+        if (isset($this->singletons[$id])) {
+            return $this->singletons[$id];
         }
 
-        // 4. Get the constructor parameters
-        $constructorParams = $constructor->getParameters();
-
-        // 5. Obtain dependencies
-        $classDependencies = $this->resolveClassDependencies($constructorParams);
-
-        // 6. Instantiate with dependencies
-        $service = $reflectionClass->newInstanceArgs($classDependencies);
-
-        // 7. Return the object
-        return $service;
-    }
-
-    private function resolveClassDependencies(array $reflectionParameters): array
-    {
-        // 1. Initialize empty dependencies array (required by newInstanceArgs)
-        $classDependencies = [];
-
-        // 2. Try to locate and instantiate each parameter
-        /** @var \ReflectionParameter $parameter */
-        foreach ($reflectionParameters as $parameter) {
-
-            // Get the parameter's ReflectionNamedType as $serviceType
-            $serviceType = $parameter->getType();
-
-            // Try to instantiate using $serviceType's name
-            $service = $this->get($serviceType->getName());
-
-            // Add the service to the classDependencies array
-            $classDependencies[] = $service;
+        // Check if it's a binding
+        if (isset($this->bindings[$id])) {
+            return $this->resolveBinding($id);
         }
 
-        // 3. Return the classDependencies array
-        return $classDependencies;
+        // If service not found in bindings, resolve via autowiring
+        return $this->resolveService($id);
     }
 
     public function has(string $id): bool
     {
-        return array_key_exists($id, $this->container);
+        return isset($this->bindings[$id]) ||
+               isset($this->singletons[$id]) ||
+               isset($this->aliases[$id]) ||
+               class_exists($id);
+    }
+
+    public function bind(string $abstract, $concrete = null, bool $shared = false): void
+    {
+        if (is_null($concrete)) {
+            $concrete = $abstract;
+        }
+
+        $this->bindings[$abstract] = [
+            'concrete' => $concrete,
+            'shared' => $shared
+        ];
+    }
+
+    public function singleton(string $abstract, $concrete = null): void
+    {
+        $this->bind($abstract, $concrete, true);
+    }
+
+    public function alias(string $abstract, string $alias): void
+    {
+        $this->aliases[$alias] = $abstract;
+    }
+
+    public function config(string $key, $value = null)
+    {
+        if (is_null($value)) {
+            return $this->parameters[$key] ?? null;
+        }
+
+        $this->parameters[$key] = $value;
+    }
+
+    public function add(string $id, $concrete = null)
+    {
+        if (is_null($concrete)) {
+            if (!class_exists($id)) {
+                throw new ContainerException("Service $id could not be added");
+            }
+            $concrete = $id;
+        }
+
+        $this->bindings[$id] = $concrete;
+    }
+
+    protected function resolveBinding(string $abstract)
+    {
+        $binding = $this->bindings[$abstract];
+
+        if ($binding instanceof \Closure) {
+            $object = $binding($this);
+        } else {
+            $object = $this->resolveService($binding);
+        }
+
+        // If the binding is shared, save it as a singleton
+        if (isset($this->bindings[$abstract]['shared']) && $this->bindings[$abstract]['shared']) {
+            $this->singletons[$abstract] = $object;
+        }
+
+        return $object;
+    }
+
+    protected function resolveService(string $class)
+    {
+        if (!class_exists($class)) {
+            throw new ContainerException("Class {$class} does not exist");
+        }
+
+        $reflector = new \ReflectionClass($class);
+
+        if (!$reflector->isInstantiable()) {
+            throw new ContainerException("Class {$class} is not instantiable");
+        }
+
+        $constructor = $reflector->getConstructor();
+
+        if (is_null($constructor)) {
+            return new $class();
+        }
+
+        $parameters = $constructor->getParameters();
+        $dependencies = $this->resolveDependencies($parameters);
+
+        return $reflector->newInstanceArgs($dependencies);
+    }
+
+    protected function resolveDependencies(array $parameters): array
+    {
+        $dependencies = [];
+
+        foreach ($parameters as $parameter) {
+            $dependency = $parameter->getType();
+
+            if (is_null($dependency)) {
+                if ($parameter->isDefaultValueAvailable()) {
+                    $dependencies[] = $parameter->getDefaultValue();
+                } else {
+                    throw new ContainerException(
+                        "Cannot resolve parameter \${$parameter->name} in {$parameter->getDeclaringClass()->getName()}"
+                    );
+                }
+            } else {
+                $dependencies[] = $this->get($dependency->getName());
+            }
+        }
+
+        return $dependencies;
+    }
+
+    public function resolveClassDependencies(array $reflectionParameters): array
+    {
+        $classDependencies = [];
+
+        foreach ($reflectionParameters as $parameter) {
+            $serviceType = $parameter->getType();
+
+            if ($serviceType) {
+                $service = $this->get($serviceType->getName());
+                $classDependencies[] = $service;
+            }
+        }
+
+        return $classDependencies;
     }
 }
+
