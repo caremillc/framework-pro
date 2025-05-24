f170c0f4750c076e25a4c969f3abd007754d6474
caremi <Willy Nilly>
2025-05-24 14:55:28 +0400

    Container Aliases for router

--- Careminate/Http/Kernel.php
+++ Careminate/Http/Kernel.php
@@ -2,44 +2,21 @@
 
 namespace Careminate\Http;
 
-use Careminate\Routing\Router;
 use Careminate\Http\Requests\Request;
 use Careminate\Http\Responses\Response;
+use Careminate\Routing\RouterInterface;
 use Careminate\Exceptions\HttpException;
 
-/**
- * HTTP Kernel
- * 
- * The Kernel class serves as the central point for handling incoming HTTP requests
- * and returning the appropriate responses by dispatching them through the router.
- */
 class Kernel
 {
-    /**
-     * Create a new Kernel instance
-     * 
-     * @param Router $router Router instance used for dispatching requests
-     */
-    public function __construct(private Router $router)
-    {
-    }
+    public function __construct(private RouterInterface $router){}
 
-    /**
-     * Handle the incoming HTTP request
-     * 
-     * Dispatches the request through the router, executes the appropriate handler,
-     * and returns the resulting response. Catches any exceptions and returns them
-     * as error responses.
-     * 
-     * @param Request $request The incoming HTTP request
-     * @return Response The HTTP response
-     */
     public function handle(Request $request): Response
     {
         try {
 
             [$routeHandler, $vars] = $this->router->dispatch($request);
-            
+
             $response = call_user_func_array($routeHandler, $vars);
 
         } catch (HttpException $exception) {
@@ -47,8 +24,7 @@ class Kernel
         } catch (\Exception $exception) {
             $response = new Response($exception->getMessage(), 500);
         }
-        
+
         return $response;
     }
 }
-
