9ad4637c94f2550104367ca27c9aab38633daf12
caremi <Willy Nilly>
2025-05-25 14:54:23 +0400

    Authenticate Middleware

--- Careminate/Http/Middlewares/Authenticate.php
+++ Careminate/Http/Middlewares/Authenticate.php
@@ -1,19 +1,26 @@
 <?php declare(strict_types=1);
 namespace Careminate\Http\Middlewares;
 
+use Careminate\Sessions\Session;
 use Careminate\Http\Requests\Request;
 use Careminate\Http\Responses\Response;
+use Careminate\Sessions\SessionInterface;
 use Careminate\Http\Middlewares\Contracts\MiddlewareInterface;
 use Careminate\Http\Middlewares\Contracts\RequestHandlerInterface;
 
 class Authenticate implements MiddlewareInterface
 {
-    private bool $authenticated = true;
+    // private bool $authenticated = true;
+     public function __construct(private SessionInterface $session){}
 
     public function process(Request $request, RequestHandlerInterface $requestHandler): Response
     {
-        if (!$this->authenticated) {
-            return new Response('Authentication failed', 401);
+        // if (!$this->authenticated) {
+        //     return new Response('Authentication failed', 401);
+        // }
+         if (!$this->session->has(Session::AUTH_KEY)) {
+            flash('error', 'Please sign in');
+            return redirect('/login');
         }
 
         return $requestHandler->handle($request);
